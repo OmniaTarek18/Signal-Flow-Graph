e03aa1aa415ae436e16a129ed6da0bd079a9b7f4
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2024-04-05 14:19:23 +0200

    clean code/ handle corner cases

--- backend/demo/src/main/java/com/example/demo/Controller.java
+++ backend/demo/src/main/java/com/example/demo/Controller.java
@@ -11,7 +11,6 @@ public class Controller {
     @PostMapping("routh")
     public String[] user(@RequestBody String equation) {
         Routh system = new RouthImp(equation);
-        System.out.println(equation);
         return system.isStable() ? new String[]{"System is stable"}:  system.rootFinder();
     }
 }
--- backend/demo/src/main/java/com/example/demo/RouthImp.java
+++ backend/demo/src/main/java/com/example/demo/RouthImp.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math3.analysis.solvers.LaguerreSolver;
 import org.apache.commons.math3.complex.Complex;
 
@@ -12,7 +10,6 @@ public class RouthImp implements Routh {
     double[] coeff;
 
     public RouthImp() {
-
     }
 
     public RouthImp(String coeff) {
@@ -27,83 +24,94 @@ public class RouthImp implements Routh {
         return coeff;
     }
 
-    // Function to parse the input to double array, aka coeff
+    // Parse the input to double array(coeff)
     private void parseInput(String equation) {
-
         equation = equation.replaceAll(" |\\[|\\]", "");
-        String []tmp = equation.split(","); 
-        this.coeff = new double[tmp.length];
-
-        for(int i = 0; i < coeff.length; i++){
-            this.coeff[i] = Double.parseDouble(tmp[i]);
+        String[] tmp = equation.split(",");
+        ArrayList<Double> handleZeros = new ArrayList<>();
+        // When entering {1,-1, 0, 0} it should be {1,-1}, As the rootfinder builtin fn
+        // enters infinity loop at {1,-1, 0, 0}
+        for (int i = 0; i < tmp.length; i++) {
+            handleZeros.add(Double.parseDouble(tmp[i]));
+        }
+        for (int i = tmp.length - 1; i >= 0; i--) {
+            if (handleZeros.get(i) != 0)
+                break;
+            handleZeros.remove(i);
+        }
+        this.coeff = new double[handleZeros.size()];
+        for (int i = 0; i < this.coeff.length; i++) {
+            this.coeff[i] = handleZeros.get(i);
         }
     }
 
+    // Check stability
     public boolean isStable() {
-        double [][]routhTable = init_routh_table(); // Initializing  routh table
-        if(routhTable[0][0] * routhTable[1][0] < 0) return false;
+        double[][] routhTable = init_routh_table(); // Initializing routh table
+        if (routhTable[0][0] * routhTable[1][0] < 0)
+            return false; // Checking the sign
+        if (routhTable[1][0] == 0)
+            routhTable[1][0] = Double.MIN_VALUE; // Handling division by zero case
         int rows = coeff.length;
         int cols = routhTable[0].length;
 
-        // Apply RH 
-        for(int i = 0; i < rows - 2; i++){
-            for(int j = 1; j < cols; j++){
-                routhTable[i+2][j - 1] = (routhTable[i + 1][0] * routhTable[i][j] - routhTable[i][0] * routhTable[i + 1][j]) / routhTable[i+1][0];
+        // Apply Routh-Hurwitz criterion
+        for (int i = 0; i < rows - 2; i++) {
+            for (int j = 1; j < cols; j++) {
+                routhTable[i + 2][j- 1] = (routhTable[i + 1][0] * routhTable[i][j] - routhTable[i][0] * routhTable[i + 1][j]) / routhTable[i + 1][0];
             }
-            if(isZeroRow(routhTable[i+2])) fixRow(i+2, routhTable); // Handling zero row case
-            if(routhTable[i+2][0] == 0) routhTable[i+2][0] = Double.MIN_VALUE; // Handling division by zero case
-            else if(routhTable[i+1][0] * routhTable[i+2][0] < 0){
-                for(int k = 0; k < rows; k++){
-                    for(int j = 0; j < cols; j++){
-                        System.out.print(routhTable[k][j]+ " ");
-                    }
-                    System.out.println();
-                }
-                return false; }// Checking the sign
-        }
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                System.out.print(routhTable[i][j]+ " ");
-            }
-            System.out.println();
+            if (isZeroRow(routhTable[i + 2]))
+                fixRow(i + 2, routhTable); // Handling zero row case
+            if (routhTable[i + 2][0] == 0)
+                routhTable[i + 2][0] = Double.MIN_VALUE; // Handling division by zero case
+            else if (routhTable[i + 1][0] * routhTable[i + 2][0] < 0)
+                return false; // Checking the sign
         }
         return true;
     }
 
+    // It is used in isStable, to check if it is a zero row (special case2)
     private boolean isZeroRow(double[] row) {
-        for(double num : row) if(num != 0) return false;
+        for (double num : row)
+            if (num != 0)
+                return false;
         return true;
     }
+
+    /*  It is used in isStable, to fix the zero row case
+     *  Description : get the derivative of the auxiliary polynomial, and replace the row with it  
+     */
 
     private void fixRow(int row, double[][] routhTable) {
         int pow = routhTable.length - row;
-        for(int i = 0; i < routhTable[0].length; i++){
+        for (int i = 0; i < routhTable[0].length; i++) {
             routhTable[row][i] = routhTable[row - 1][i] * pow;
             pow -= 2;
         }
     }
-
-    private double[][]  init_routh_table() {
+   
+    // It is used in isStable, fill the first two rows with the coeff before starting 
+    private double[][] init_routh_table() {
         int rows = coeff.length;
-        int cols = (rows - 1) / 2 + 1;    // Get number of cols
+        int cols = (rows - 1) / 2 + 1; // Get number of cols
         double[][] routhTable = new double[rows][cols];
         // Set the first two rows
         int firstRowCol = 0, secRowCol = 0;
-        for(int i = coeff.length - 1; i > -1; i--){
-            if(firstRowCol == secRowCol) routhTable[0][firstRowCol++] = coeff[i];
-            else routhTable[1][secRowCol++] = coeff[i];
+        for (int i = coeff.length - 1; i > -1; i--) {
+            if (firstRowCol == secRowCol)
+                routhTable[0][firstRowCol++] = coeff[i];
+            else
+                routhTable[1][secRowCol++] = coeff[i];
         }
         return routhTable;
     }
 
+    // Finds the roots in the RHS
     public String[] rootFinder() {
-        PolynomialFunction poly = new PolynomialFunction(coeff);
-        System.out.println(poly);
         // Create a solver
         LaguerreSolver solver = new LaguerreSolver();
         // Find the roots
-        ArrayList<Complex> roots = new ArrayList<>(Arrays.asList(solver.solveAllComplex(coeff, 0))); // Finds all complex roots
-                                                                                                      
+        ArrayList<Complex> roots = new ArrayList<>(Arrays.asList(solver.solveAllComplex(coeff, 0))); 
         for (int i = 0; i < roots.size(); i++) {
             if (roots.get(i).getReal() < 0)
                 roots.remove(i--); // Remove the LHS roots
@@ -111,12 +119,13 @@ public class RouthImp implements Routh {
         return parseOutput(roots);
     }
 
+    // Print o/p
     private String[] parseOutput(ArrayList<Complex> roots) {
         StringBuilder str = new StringBuilder();
         str.append("System is unstable,");
         str.append("Number of roots in the RHS: ");
         str.append(roots.size());
-        for(Complex root: roots){
+        for (Complex root : roots) {
             str.append(",");
             str.append("Real part: ");
             str.append(String.format("%.4g", root.getReal()));
@@ -126,5 +135,4 @@ public class RouthImp implements Routh {
         }
         return str.toString().split(",");
     }
-
 }
